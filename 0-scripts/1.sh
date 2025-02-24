34cd534b1774f5ea02a56a6b3fcd60c50165f7b9
roysubs <roysubs@hotmail.com>
2025-02-24 01:21:35 +0000

    Various

--- 0-scripts/1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Your script logic here...
-
-# Append the desired text to the next prompt
-printf "\n"  # Ensure a clean newline
-PROMPT_COMMAND='printf "# . ~/.bashrc"; PROMPT_COMMAND=""'
-
