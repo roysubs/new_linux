acf6f5f7d8040355e93a9c4e33f1568f5c31d124
roysubs <roysubs@hotmail.com>
2025-02-27 23:13:04 +0100

    Various

---
+++ 0-help/x-glow-template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! command -v glow >/dev/null 2>&1; then
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+    sudo apt update && sudo apt install glow
+fi
+
+cat <<'EOF' | glow -p -w $(( $(tput cols) - 6 ))
+
+...
+###
+### Put Markdown text here
+###
+...
+
+EOF
+
