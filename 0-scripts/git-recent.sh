fc74bc8b0cef7484f0e9db2655bd8353112a1ff2
roysubs <roysubs@hotmail.com>
2025-04-03 16:04:54 +0200

    Various

--- 0-scripts/git-recent.sh
+++ 0-scripts/git-recent.sh
@@ -3,44 +3,6 @@
 # Set default number of commits to show (5)
 num_commits=${1:-5}
 
-# Get the root directory of the Git repository
-GIT_REPO_PATH=$(git rev-parse --show-toplevel)
-POST_RECEIVE_HOOK="$GIT_REPO_PATH/.git/hooks/post-receive"
-
-# Check if the post-receive hook exists
-if [ -f "$POST_RECEIVE_HOOK" ]; then
-    # If the hook exists, check if it is non-empty
-    if [ -s "$POST_RECEIVE_HOOK" ]; then
-        echo "Git push events are being recorded. Here is the content of the post-receive hook:"
-        cat "$POST_RECEIVE_HOOK"
-    else
-        echo "The post-receive hook exists, but it is empty. No push events are being recorded."
-        read -p "Would you like to populate the post-receive hook to record push events? (y/n): " response
-        if [[ "$response" =~ ^[Yy]$ ]]; then
-            # Add a basic logging script to the post-receive hook
-            echo "#!/bin/bash" > "$POST_RECEIVE_HOOK"
-            echo "echo \"\$(date) - Push received on branch \$(git rev-parse --abbrev-ref HEAD)\" >> $GIT_REPO_PATH/push_log.txt" >> "$POST_RECEIVE_HOOK"
-            chmod +x "$POST_RECEIVE_HOOK"
-            echo "The post-receive hook has been populated and is now recording push events."
-        else
-            echo "The post-receive hook was not modified. Push events will not be recorded."
-        fi
-    fi
-else
-    # If the post-receive hook doesn't exist
-    echo "No post-receive hook found. Git push events are not being recorded."
-    read -p "Would you like to create a post-receive hook to record push events? (y/n): " response
-    if [[ "$response" =~ ^[Yy]$ ]]; then
-        # Create a new post-receive hook to log push events
-        echo "#!/bin/bash" > "$POST_RECEIVE_HOOK"
-        echo "echo \"\$(date) - Push received on branch \$(git rev-parse --abbrev-ref HEAD)\" >> $GIT_REPO_PATH/push_log.txt" >> "$POST_RECEIVE_HOOK"
-        chmod +x "$POST_RECEIVE_HOOK"
-        echo "The post-receive hook has been created and is now recording push events."
-    else
-        echo "No post-receive hook was created. Push events will not be recorded."
-    fi
-fi
-
 echo "=== Last $num_commits Commits ==="
 git log -n $num_commits --pretty=format:"%cd %h - %s" --date=iso
 echo
@@ -66,13 +28,6 @@ for commit in $(git log -n $num_commits --pretty=format:"%h"); do
         esac
     done
     echo
-done
-
-echo "=== Last $num_commits Push Actions ==="
-# Find last n pushes from reflog and show timestamps
-git reflog show --date=iso -n $num_commits | grep -i "push" | while read line; do
-    # Extract and show the timestamp and reference for each push event
-    echo "$line" | sed -E 's/^([0-9-]+ [0-9:]+).*/\1 - push event/'
 done
 
 echo
