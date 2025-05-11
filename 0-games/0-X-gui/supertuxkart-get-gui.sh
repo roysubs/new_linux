37b546b4990391f96746c60978f61e7f83bacb88
roysubs <roysubs@hotmail.com>
2025-05-11 16:26:53 +0200

    Various

---
+++ 0-games/0-X-gui/supertuxkart-get-gui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "A fun, kart-racing game similar to Mario Kart, with a variety of tracks and characters from open-source projects."
+echo "Works well on WSL in Windows (with WSLg)"
+
+sudo apt install supertuxkart
+
